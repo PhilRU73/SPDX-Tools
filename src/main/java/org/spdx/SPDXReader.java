340f2e0d11f67d3d379356c52ce5f6619bc9069d
Edo Shor <edo.shor@whitesourcesoftware.com>
2013-04-08 17:54:31 +0300

    Reader facade + tests

---
+++ src/main/java/org/spdx/SPDXReader.java
@@ -0,0 +1,115 @@
+/**
+* Copyright (c) 2013 White Source ltd.
+*
+*   Licensed under the Apache License, Version 2.0 (the "License");
+*   you may not use this file except in compliance with the License.
+*   You may obtain a copy of the License at
+*
+*       http://www.apache.org/licenses/LICENSE-2.0
+*
+*   Unless required by applicable law or agreed to in writing, software
+*   distributed under the License is distributed on an "AS IS" BASIS,
+*   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*   See the License for the specific language governing permissions and
+*   limitations under the License.
+*
+*/
+package org.spdx;
+
+import antlr.RecognitionException;
+import antlr.TokenStreamException;
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+import org.spdx.rdfparser.InvalidSPDXAnalysisException;
+import org.spdx.rdfparser.SPDXDocument;
+import org.spdx.rdfparser.SPDXDocumentFactory;
+import org.spdx.spdxspreadsheet.SPDXSpreadsheet;
+import org.spdx.spdxspreadsheet.SpreadsheetException;
+import org.spdx.spdxspreadsheet.SpreadsheetToRDF;
+import org.spdx.tag.BuildDocument;
+import org.spdx.tag.CommonCode;
+import org.spdx.tag.TagValueLexer;
+import org.spdx.tag.TagValueParser;
+
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ * One stop shop for all format conversion functionality.
+ *
+ * @author Edo.Shor
+ */
+public class SPDXReader {
+
+    /**
+     * Read SPDX Document from a RDFa file.
+     *
+     * @param rdfFile RDFa file location.
+     *
+     * @return Analysis of the given file.
+     *
+     * @throws IOException In case of errors reading the given file.
+     * @throws InvalidSPDXAnalysisException In case of errors in the file format.
+     */
+    public static SPDXDocument readRdf(File rdfFile) throws IOException, InvalidSPDXAnalysisException {
+        return SPDXDocumentFactory.creatSpdxDocument(rdfFile.getAbsolutePath());
+    }
+
+    /**
+     * Read SPDX Document from an Excel spreadsheet file.
+     *
+     * @param ssFile Excel file location.
+     *
+     * @return Analysis of the given file.
+     *
+     * @throws IOException In case of errors reading the given file.
+     * @throws InvalidSPDXAnalysisException In case of errors in the file format.
+     * @throws SpreadsheetException In case of error in the spreadsheet format.
+     */
+    public static SPDXDocument readSpreadsheet(File ssFile) throws IOException, InvalidSPDXAnalysisException, SpreadsheetException {
+        Model model = ModelFactory.createDefaultModel();
+        SPDXDocument analysis = new SPDXDocument(model);
+        SPDXSpreadsheet ss = new SPDXSpreadsheet(ssFile, false, true);
+        SpreadsheetToRDF.copySpreadsheetToSPDXAnalysis(ss, analysis);
+        return analysis;
+    }
+
+    /**
+     * Read SPDX Document from a tag value file.
+     *
+     * @param tagFile Tag value file location.
+     *
+     * @return Analysis of the given file.
+     *
+     * @throws IOException In case of errors reading the given file.
+     * @throws InvalidSPDXAnalysisException In case of errors in the file SPDX format.
+     * @throws RecognitionException In case of errors in the tag value format.
+     * @throws TokenStreamException
+     * @throws Exception In case of an unchecked exception while parsing the tag value file.
+     */
+    public static SPDXDocument readTagFile(File tagFile) throws Exception{
+        SPDXDocument analysis;
+
+        FileInputStream fis = null;
+        try {
+            fis = new FileInputStream(tagFile);
+            TagValueLexer lexer = new TagValueLexer(new DataInputStream(fis));
+            TagValueParser parser = new TagValueParser(lexer);
+            Model model = ModelFactory.createDefaultModel();
+            analysis = new SPDXDocument(model);
+            Properties constants = CommonCode.getTextFromProperties("SpdxTagValueConstants.properties");
+            parser.setBehavior(new BuildDocument(model, analysis, constants));
+            parser.data();
+        } finally {
+            if (fis != null) {
+                fis.close();
+            }
+        }
+
+        return analysis;
+    }
+
+}
---
+++ src/test/java/org/spdx/SPDXReaderTest.java
@@ -0,0 +1,62 @@
+/**
+ * Copyright (c) 2013 White Source ltd.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ */
+package org.spdx;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.spdx.rdfparser.InvalidSPDXAnalysisException;
+import org.spdx.rdfparser.SPDXDocument;
+import org.spdx.spdxspreadsheet.SpreadsheetException;
+
+import java.io.File;
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * Unit test for the
+ *
+ * @author Edo.Shor
+ */
+public class SPDXReaderTest {
+
+    @Test
+    public void testReadRdf() throws IOException, InvalidSPDXAnalysisException {
+        File file = new File("Examples/SPDXRdfExample.rdf").getAbsoluteFile();
+        SPDXDocument document = SPDXReader.readRdf(file);
+        assertNotNull(document);
+        assertEquals(0, document.verify().size());
+    }
+
+    @Test
+    public void testReadTag() throws Exception {
+        File file = new File("Examples/SPDXTagExample.tag").getAbsoluteFile();
+        SPDXDocument document = SPDXReader.readTagFile(file);
+        assertNotNull(document);
+        assertEquals(0, document.verify().size());
+    }
+
+    @Ignore
+    @Test
+    public void testReadSpreadsheet() throws IOException, InvalidSPDXAnalysisException, SpreadsheetException {
+        File file = new File("Examples/SPDXSpreadsheetExample.xls").getAbsoluteFile();
+        SPDXDocument document = SPDXReader.readSpreadsheet(file);
+        assertNotNull(document);
+        assertEquals(0, document.verify().size());
+    }
+}
